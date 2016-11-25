685fee29ef6011c2971e6110b0590e14dfae891a
ErnestoGonAr <ergonzalezar@ittepic.edu.mx>
2016-11-24 23:16:26 -0600

    Comunicacion de InterfazCita con InterfazMapa

--- app/src/main/java/com/example/ernesto/beep/InterfazCita.java
+++ app/src/main/java/com/example/ernesto/beep/InterfazCita.java
@@ -3,6 +3,7 @@ package com.example.ernesto.beep;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by ernesto on 23/11/16.
@@ -13,6 +14,10 @@ public class InterfazCita extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.interfazcita);
+    }
+
+    public void mostrarMapa(View view){
+        setContentView(R.layout.activity_maps);
     }
 
 }
